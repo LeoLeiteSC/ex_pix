3a6a61d03a6392e3c01695e22e72e66594f4f1a6
Leonardo Leite <leonardo.leitesc@gmail.com>
2021-04-15 19:45:51 -0300

    Add configuration file with the basic values

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+# General application configuration
+import Config
+
+config :ex_pixqr, :static,
+  payload_indicator: "01",
+  initiation_method: "11",
+  gui: "br.gov.bcb.pix",
+  currency_code: "986",
+  country_code: "BR"
