0143a921a2cc10d73a43efc210c18785b03c9335
Leonardo Leite <leonardo.leitesc@gmail.com>
2021-04-15 21:41:22 -0300

    Create function to return a final static code

--- lib/ex_pixqr.ex
+++ lib/ex_pixqr.ex
@@ -3,16 +3,32 @@ defmodule ExPIXQR do
   Documentation for `ExPIXQR`.
   """
 
-  @doc """
-  Hello world.
+  alias ExPIXQR.Data.{
+    Additional,
+    Static,
+    StaticAccount,
+  }
+  alias ExPIXQR.Code.Builder
 
-  ## Examples
+  @type static_params :: %{
+    required(:merchant_name) => String.t,
+    required(:merchant_city) => String.t,
+    optional(:postal_code) => String.t,
+    optional(:amount) => String.t,
+    optional(:initiation_method) => String.t,
+  }
 
-      iex> ExPIXQR.hello()
-      :world
+  @type account_params :: %{optional(:info) => String.t}
+  @type additional_params :: %{optional(:txid) => String.t}
 
-  """
-  def hello do
-    :world
+  @spec generate_static_code(String.t(), static_params, account_params, additional_params) :: {:ok, String.t()} | {:error, any}
+  def generate_static_code(key, static_params, account_params \\ %{}, additional_params \\ %{}) do
+    account_info = Map.merge(%StaticAccount{}, Map.merge(%{key: key}, account_params))
+    additional_info = Map.merge(%Additional{}, additional_params)
+    final_params = static_params |> Map.put(:account_information, account_info) |> Map.put(:additional, additional_info)
+
+    %Static{}
+    |> Map.merge(final_params)
+    |> Builder.build()
   end
 end
--- test/ex_pixqr_test.exs
+++ test/ex_pixqr_test.exs
@@ -2,7 +2,28 @@ defmodule ExPIXQRTest do
   use ExUnit.Case
   doctest ExPIXQR
 
-  test "greets the world" do
-    assert ExPIXQR.hello() == :world
+  alias ExPIXQR.Data.Static
+
+  setup [:build_params]
+
+  describe "when parameters are correct" do
+    test "should build a code if the parameters are correct", %{static_params: static_params} do
+      assert {:ok, received_string} = ExPIXQR.generate_static_code("random_key", static_params)
+
+      assert is_binary(received_string)
+    end
+  end
+
+  describe "when parameters are incorrect" do
+    test "should return an error if a parameter is incorrect", %{static_params: static_params} do
+      name_limit = Static.max_lengths().merchant_name
+      new_params = Map.replace!(static_params, :merchant_name, String.pad_leading("a", name_limit + 1, "a"))
+
+      assert {:error, "The merchant_name is invalid"} == ExPIXQR.generate_static_code("random_key", new_params)
+    end
+  end
+
+  defp build_params(_) do
+    [static_params: %{merchant_name: "Name", merchant_city: "City"}]
   end
 end
