85fbed588a6ab0563e038a23f11194a027ec1b9f
Leonardo Leite <leonardo.leitesc@gmail.com>
2021-04-19 22:22:51 -0300

    Add new configuration parameters to the mix file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,22 @@ defmodule ExPIX.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "ExPIX",
+      source_url: "https://github.com/LeoLeiteSC/ex_pix",
+      description: "Elixir library to deal with PIX payments",
+      package: package(),
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/LeoLeiteSC/ex_pix",
+        "Docs" => "https://hexdocs.pm/ex_pix/"
+      }
     ]
   end
 
@@ -19,6 +34,9 @@ defmodule ExPIX.MixProject do
 
   defp deps do
     [
+      # Documentation
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      # QR-Codes
       {:eqrcode, "~> 0.1.7"},
       {:crc, "~> 0.10.1"},
     ]
